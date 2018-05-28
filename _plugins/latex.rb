174477d8661e9b08903c291f9cd9e8de38689ce9
Michael Zhang <failed.down@gmail.com>
2018-05-28 12:14:02 -0500

    get rid of useless latex parsing code

--- _plugins/latex.rb
+++ _plugins/latex.rb
@@ -1,33 +1,5 @@
 require "mathematical"
 
-# module Jekyll
-# 	class LatexGenerator < Generator
-# 		safe true
-# 		def generate(site)
-# 			blockRgx = /\$\$[^\$]+\$\$/
-# 			site.posts.each do |post|
-# 				buf = StringIO.new
-# 				prev = 0
-# 				post.content.to_enum(:match, blockRgx).with_index.each do |match, i|
-# 					a, b = match.offset(i)
-# 					buf << post.content[prev..a]
-					
-# 					render = Mathematical.new.render(match[i])
-# 					buf << render[:data]
-
-# 					prev = b
-# 				end
-# 				buf << post.content[prev..post.content.length]
-# 				p buf.string
-# 			end
-# 			inlineRgx = /[^\$]\$[^\$]+\$[^\$]/
-# 			site.posts.each do |post|
-# 				p post.content.scan(inlineRgx)
-# 			end
-# 		end
-# 	end
-# end
-
 $blockRgx = /((?<block>\$\$[^\$]+\$\$)|[^\$](?<inline>\$[^\$]+\$)[^\$])/
 
 module Jekyll
