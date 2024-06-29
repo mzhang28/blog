f83ee810b56c4e26ac335dcc4584550f62df60d3
Michael Zhang <mail@mzhang.io>
2024-06-29 14:04:50 -0500

    a

--- flake.nix
+++ flake.nix
@@ -1,26 +1,34 @@
 {
   inputs.agda.url = "github:agda/agda";
-  outputs = { self, nixpkgs, flake-utils, agda }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      agda-pkg = agda.packages.x86_64-linux.default;
-      flakePkgs = rec {
-        agda-bin = pkgs.callPackage ./nix/agda-bin.nix { inherit agda-pkg; };
-        docker-builder = pkgs.callPackage ./nix/docker-builder.nix { inherit agda-bin; };
-      };
-    in
-    {
-      packages = flake-utils.lib.flattenTree flakePkgs;
-      devShell = pkgs.mkShell {
-        packages = with pkgs; [
-          bun
-          woodpecker-cli
-          nixfmt-rfc-style
-          dive
-          nix-tree
-          vips
-          shellcheck
-        ];
-      };
-    });
+  outputs = { self, nixpkgs, flake-utils, agda, }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        agda-pkg = agda.packages.x86_64-linux.default;
+        flakePkgs = rec {
+          agda-bin = pkgs.callPackage ./nix/agda-bin.nix { inherit agda-pkg; };
+          docker-builder =
+            pkgs.callPackage ./nix/docker-builder.nix { inherit agda-bin; };
+        };
+      in {
+        packages = flake-utils.lib.flattenTree flakePkgs;
+        devShell = pkgs.mkShell {
+          ASTRO_TELEMETRY_DISABLED = 1;
+
+          packages = with pkgs;
+            with flakePkgs; [
+              bun
+              woodpecker-cli
+              nixfmt-rfc-style
+              dive
+              nix-tree
+              vips
+              shellcheck
+              agda-bin
+
+              nodejs_20
+              corepack
+            ];
+        };
+      });
 }
