f92086d2fab620c9648c611ab500d11e5a9cff55
Michael Zhang <mail@mzhang.io>
2024-10-22 22:10:58 -0500

    update rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,41 @@
 import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
+const BLACKLIST = ["../content/posts/_index.md"];
+
 export async function GET(context) {
-  const posts = await getCollection("posts");
+  // const posts = Array.from(await getCollection("posts"));
+  const posts = Object.entries(
+    import.meta.glob("../content/posts/**/*.{md,mdx}", {
+      eager: true,
+    }),
+  )
+    .filter(
+      ([file, post]) =>
+        !BLACKLIST.includes(file) && post.frontmatter.draft !== true,
+    )
+    .toSorted(([fileA, a], [fileB, b]) => {
+      return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
+    })
+    .slice(0, 30)
+    .map(([file, post]) => {
+      console.log("post", { file, post });
+      return {
+        title: post.frontmatter.title ?? "",
+        pubDate: new Date(post.frontmatter.date),
+        link: "/",
+        content: sanitizeHtml(post.compiledContent?.()),
+        // link: `/posts/${post.slug}/`,
+      };
+    });
+
+  console.log("posts", posts);
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.date,
-      //   ...post.data,
-      link: `/${post.slug}/`,
-    })),
+    items: posts,
   });
 }
