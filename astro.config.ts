06571fc4f0d4e13131f556737a4b17a6d195b345
Michael Zhang <mail@mzhang.io>
2025-10-21 03:55:58 -0400

    update config

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,17 @@
-import { defineConfig } from "astro/config";
+import markdoc from "@astrojs/markdoc";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
-import remarkReadingTime from "./plugin/remark-reading-time";
-import remarkEmoji from "remark-emoji";
-import remarkDescription from "astro-remark-description";
-import remarkAdmonitions, { mkdocsConfig } from "./plugin/remark-admonitions";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
-import remarkTypst from "./plugin/remark-typst";
 import rehypeLinkHeadings from "@justfork/rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
-
-import markdoc from "@astrojs/markdoc";
-import remarkAgda from "./plugin/remark-agda";
-
 import tailwindcss from "@tailwindcss/vite";
+import remarkDescription from "astro-remark-description";
+import { defineConfig } from "astro/config";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
+import rehypeKatex from "rehype-katex";
+import rehypeSlug from "rehype-slug";
+import remarkEmoji from "remark-emoji";
+import remarkMath from "remark-math";
+import remarkAdmonitions, { mkdocsConfig } from "./plugin/remark-admonitions";
+import remarkReadingTime from "./plugin/remark-reading-time";
 
 const outDir = "dist";
 const base = process.env.BASE ?? "/";
@@ -36,8 +32,6 @@ export default defineConfig({
     syntaxHighlight: "shiki",
     shikiConfig: { theme: "css-variables" },
     remarkPlugins: [
-      remarkTypst,
-      () => remarkAgda({ outDir, base, publicDir }),
       [remarkMath, {}],
       [remarkAdmonitions, mkdocsConfig],
       remarkReadingTime,
